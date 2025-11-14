9dfca76795bb04061852288749a73109dd23b8fc
Daniel Silva <165176862+Daniel13s@users.noreply.github.com>
2025-11-14 16:35:41 -0300

    Update AboutMe component description

--- src/app/components/AboutMe/AboutMe.tsx
+++ src/app/components/AboutMe/AboutMe.tsx
@@ -4,10 +4,10 @@ export default function AboutMe() {
     return (
         <div className='aboutMe'>
             <h1>Sobre Mim</h1>
-            <p>Sou um desenvolvedor Front-End apaixonado por transformar ideias em interfaces funcionais, responsivas e atraentes. Tenho foco em HTML, CSS/TailWind, JavaScript/TypeScript e React/Next.js, buscando sempre unir boa experiência do usuário com código limpo e eficiente.
+            <p>Sou um Web desenvolvedor apaixonado por transformar ideias em interfaces funcionais, responsivas e atraentes, atualmente resíduo em Cajazeiras-PB. Tenho foco em HTML, CSS/TailWind, JavaScript/TypeScript e React/Next.js, buscando sempre unir boa experiência do usuário com código limpo e eficiente.
 <br />
 <br />
-Gosto de resolver problemas, aprender novas tecnologias e acompanhar as tendências do mundo web. Atualmente, estou construindo projetos pessoais que reforçam minhas habilidades em componentização, gerenciamento de estado, integração com APIs e design responsivo.
+Gosto de resolver problemas, aprender novas tecnologias e acompanhar as tendências no mundo do web-desenvolvimento. Atualmente, estou construindo projetos pessoais que reforçam minhas habilidades em componentização, gerenciamento de estado, integração com APIs e design responsivo.
 <br /><br />
 Quando não estou codando, estou estudando novas formas de melhorar a performance, acessibilidade e usabilidade das aplicações que desenvolvo.</p>
         </div>
